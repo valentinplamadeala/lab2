e2e841233630a1c707b33a62a8ef7e67264cec42
valentinplamadeala <valentin.plamadeala@student.upt.ro>
2024-02-29 14:25:59 +0200

    al 3 lea commit

--- src/p1/main.java
+++ src/p1/main.java
@@ -1,36 +1,30 @@
 package p1;
-import java.util.Scanner;
 import java.util.*;
-import java.util.Arrays;
 
 public class main {
-    public static void afisare(String []judete)
-    {
-        for(int i = 0; i< judete.length; i++)
-        {
-            System.out.println(judete[i] + " ");
-        }
-        System.out.println();
-    }
     public static void main(String[] args) {
+
         int i = 0;
         String nume_fis = "\\src\\p1\\judete_in.txt";
         Scanner in = new Scanner(nume_fis);
-        String[] judete = new String [70];
+        String[] judete = new String [41];
+
         while(in.hasNext())
         {
-            judete[i] = in.toString();
+            judete[i] = in.nextLine();
             i = i + 1;
         }
-        Arrays.sort(judete);
+
+        Arrays.sort(judete,0,i-1);
+
         Scanner scanner = new Scanner(System.in);
         String judet_cautat;
         System.out.print("Dati judetul: ");
         judet_cautat = scanner.toString();
+
         System.out.println();
         int poz = Arrays.binarySearch(judete, judet_cautat);
         System.out.println("Judetul cautat se afla pe pozitia: " + poz);
-
 
     }
 }
---
+++ src/p2/main2.java
@@ -0,0 +1,8 @@
+package p2;
+import java.util.*;
+
+public class main2 {
+    public static void main(String[] args) {
+
+    }
+}
